6b55225ac4de0c07b5cf3025d37117573201c383
magedkamel110 <anas.massadgit0@gmail.com>
2025-02-08 13:58:09 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JR96XP2');</script>
+<!-- End Google Tag Manager -->
+  
         <link rel="icon" type="image/png" href="/main_logo.png" />
         <link
           rel="stylesheet"
@@ -37,11 +44,10 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
-     <!-- Google Tag Manager (noscript) -->
+<!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JR96XP2"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-
  </body>
     </Html>
   );
