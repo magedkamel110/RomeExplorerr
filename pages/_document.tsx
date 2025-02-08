cfdccf72fffc31b71c0ee63da72de3d88db061b1
Your Name <anasmuostafa@gmail.com>
2025-02-08 17:25:31 +0200

    add clarity setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -34,6 +35,15 @@ export default function Document() {
         </noscript>
         <Main />
         <NextScript />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "q6lmwpgsmo");
+          `}
+        </Script>
       </body>
     </Html>
   );
