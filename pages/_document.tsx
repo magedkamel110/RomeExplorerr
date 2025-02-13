7bbadb1f45c83b4606ac9adea30bdc97bf14c56c
magedkamel110 <anas.massadgit0@gmail.com>
2025-02-13 14:09:47 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','YOUR_GTM_ID');
+    `,
+  }}
+/>
   {/* Google tag (gtag.js) */}
   <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16862142591"></script>
   <script
@@ -60,6 +71,16 @@ export default function Document() {
             })(window, document, "clarity", "script", "q6lmwpgsmo");
           `}
         </Script>
+
+          <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=YOUR_GTM_ID"
+    height="0"
+    width="0"
+    style={{ display: 'none', visibility: 'hidden' }}
+  />
+</noscript>
+
       </body>
     </Html>
   );
